adede54dbe4a378b20733de27cc3a85d16dd6a13
dojo24 <mlongenberger@codingdojo.com>
2021-06-10 23:53:47 -0400

    saving changes

--- expressApp/models.py
+++ expressApp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+STATUS = (
+    (0,"Draft"),
+    (1,"Publish")
+)
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blogPosts')
+    updatedOn = models.DateTimeField(auto_now= True)
+    content = models.TextField()
+    createdOn = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['-createdOn']
+
+    def __str__(self):
+        return self.title
