c4d65923941a519285d21cf3620ebcaafd6eff73
dojo24 <melissalongenberger24@gmail.com>
2021-06-12 10:20:07 -0400

    updating app

--- expressApp/models.py
+++ expressApp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 STATUS = (
     (0,"Draft"),
     (1,"Publish")
@@ -9,7 +10,7 @@ STATUS = (
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blogPosts')
+    author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
     updatedOn = models.DateTimeField(auto_now= True)
     content = models.TextField()
     createdOn = models.DateTimeField(auto_now_add=True)
